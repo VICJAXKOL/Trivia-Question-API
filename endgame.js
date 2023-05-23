9540cc6317c7c75d336a189447390cd8f5b0fbae
Victor Thompson <kelechithompson26@gmail.com>
2023-05-23 12:56:00 +0100

    add top five scores function

---
+++ endgame.js
@@ -0,0 +1,51 @@
+let username =document.getElementById('username-input');
+let save =document.getElementById('save') ;
+let finaleScore =document.getElementById('finalScore') ;
+let resume =document.getElementById('resume') ;
+let mostRecentScore = localStorage.getItem('mostRecentScore') ;
+let highScores = JSON.parse(localStorage.getItem('scores'))|| [] ;
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+
+const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+
+
+/* TOP 5 SCORES */
+const MAX_SCORES =4 ;
+
+save.addEventListener('click', (e) => {
+    if(username.value === "") {
+        alert('You must fill the username to save ', 'danger')
+        return 
+    }
+    highScores.push({
+        "username" :username.value , 
+        "score":localStorage.getItem('mostRecentScore')
+    }) ;
+
+    highScores.sort((a,b) => b.score-a.score) ;
+
+    highScores.splice(5) ;
+    
+    localStorage.setItem('scores',JSON.stringify(highScores)) ;
+    username.value ="" ;
+
+    e.preventDefault() ;
+
+    alert('Result saved', 'success')
+    
+})
+
+
+
